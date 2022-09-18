312e1c339ce8695e73ca0d53e81c68c4c6c02444
Dmytro_Kornieiev <dmytro.kornieiev@wickes.co.uk>
2022-09-18 23:05:37 +0300

    Spring task.

---
+++ src/main/java/com/epam/task/spring/SpringXMLConfigurationMain.java
@@ -0,0 +1,16 @@
+package com.epam.task.spring;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class SpringXMLConfigurationMain {
+    public static void main( String[] args )
+    {
+        ClassPathXmlApplicationContext ac = new ClassPathXmlApplicationContext("ApplicationContext.xml");
+        Country countryBean = (Country) ac.getBean("country");
+        String name = countryBean.getName();
+        System.out.println("Country name: "+name);
+        long population = countryBean.getPopulation();
+        System.out.println("Country population: "+population);
+        ac.close();
+    }
+}
